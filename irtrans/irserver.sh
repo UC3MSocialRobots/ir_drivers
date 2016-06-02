54701bdf4844a4b5364c187a2fa0076602ced3dd
Raúl Pérula-Martínez <raules@gmail.com>
2016-06-02 10:22:32 +0200

    fixed bug in pkg name

--- irtrans/irserver.sh
+++ irtrans/irserver.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd `rospack find maggie_ir_drivers`/third_parties/irserver/
+cd `rospack find ir_trans_drivers`/irserver/
 
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == "x86_64" ]; then
-  EXE=./irserver64
+    EXE=./irserver64
 else
-  EXE=./irserver
+    EXE=./irserver
 fi
 
 ${EXE} /dev/ttyUSBir
